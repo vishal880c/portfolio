7ccb794d1b88be736157a5f361807ea6d1add212
vishal kumar <vishalkr3R@gmail.com>
2025-09-28 13:14:19 +0530

    now

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
-import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
-import About from './Components/About'
-import Contact from './Components/Contact'
-import Footer from './Components/Footer'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 
 
